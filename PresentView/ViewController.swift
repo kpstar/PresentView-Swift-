250d8b9f778b79b5b5c4661044b423ee593289d2
KpStar <kpstar@kpstars-mac.local>
2018-05-08 11:31:22 +0300

    Initial Commit

---
+++ PresentView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PresentView
+//
+//  Created by KpStar on 5/8/18.
+//  Copyright © 2018 freelancer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
